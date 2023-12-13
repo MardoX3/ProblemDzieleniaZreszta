64f186feb0304e5a424de30b6bf76a809852f053
MardoX <wiktorprzylucki3@wp.pl>
2023-12-13 08:05:11 +0100

    e

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] moneyArray = {50000,20000,10000,5000,2000,1000,500,200,100,50,28,10,5,2,1};
         System.out.println("Algorytm wydawania reszty");
-        System.out.println("Kwota do zaplaty");
-        int doZaplaty = (int) scanner.nextDouble()*100;
-        System.out.println("kwota ziutka");
-        int ileMa = (int) scanner.nextDouble()*100;
-        int rest = ileMa%doZaplaty;
-        int[] wynik = givingChange(rest,moneyArray);
-        for (int i = 0;i < wynik.length; i++){
-            System.out.print((moneyArray[i]/100)+" - "+wynik[i]+"| ");
+        System.out.println("Jaka kwote masz do zaplaty");
+        int zaplata = (int) ((scanner.nextDouble()) * 100);
+        System.out.println("Ile pieniędzy dałes");
+        int kwotaWplacona = (int) ((scanner.nextDouble()) * 100);
+        int [] moneyArray = {50000,20000,10000,5000,2000,1000,500,200,100,5,2,1};
+        int rest = kwotaWplacona-zaplata;
+        int [] reszta = givingChange(moneyArray,rest);
+        for (int i = 0; i<reszta.length;i++) {
+            System.out.println("Banknot o wartosci "+((double)(moneyArray[i]))/100+" jest tutaj w ilosci: "+reszta[i]);
         }
     }
-    public static int[] givingChange(int rest, int[] moneyArray){
+
+    public static int[] givingChange(int[] moneyArray,int rest){
         int size = moneyArray.length;
         int[] amountOfMoney = new int[size];
-        for(int i = 0; i < size; i++){amountOfMoney[i] = 0;}
+        for (int i=0;i<size;i++) amountOfMoney[i] = 0;
         int i=0;
-        while(rest >0){
+
+        while (rest>0) {
             amountOfMoney[i] = rest/moneyArray[i];
-            rest -= amountOfMoney[i]* moneyArray[i];
+            rest -= amountOfMoney[i]*moneyArray[i];
             i++;
         }
         return amountOfMoney;
-
     }
 }
